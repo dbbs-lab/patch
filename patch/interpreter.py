0cc78b6b8029bb275707cdfc535caf0e9d335f62
Robin De Schepper <robin.deschepper93@gmail.com>
2019-12-10 12:20:05 +0100

    fixed to comply with upcomoing tests

--- patch/interpreter.py
+++ patch/interpreter.py
@@ -1,4 +1,6 @@
-from .objects import PythonHocObject
+from .objects import PythonHocObject, NetCon
+from .core import transform
+from .exceptions import *
 
 class PythonHocInterpreter:
   def __init__(self):
@@ -14,19 +16,27 @@ class PythonHocInterpreter:
     # object instead.
     attr = getattr(self.__h, attr_name)
     if self.__requires_wrapping:
-      return self.wrap(attr)
+      return self.wrap(attr, attr_name)
     else:
       return attr
 
-  def wrap(self, factory):
+  def wrap(self, factory, name):
     def wrapper(*args, **kwargs):
       obj = factory(*args, **kwargs)
-      return PythonHocObject(self.__h, obj)
+      cls = next((c for c in self.__object_classes if c.__name__ == name), None)
+      return cls(self.__h, obj)
     return wrapper
 
   def NetCon(self, source, target, *args, **kwargs):
+    nrn_source = transform(source)
+    nrn_target = transform(target)
     connection = NetCon(self, self.__h.NetCon(nrn_source, nrn_target, *args, **kwargs))
     connection.__ref__(self)
     connection.__ref__(target)
+    if not hasattr(source, "_connections"):
+      raise NotConnectableError('Source ' + str(source) + ' is not connectable. It lacks attribute _connections required to form NetCons.')
+    if not hasattr(target, "_connections"):
+      raise NotConnectableError('Target ' + str(target) + ' is not connectable. It lacks attribute _connections required to form NetCons.')
     source._connections[target] = connection
     target._connections[source] = connection
+    return connection
