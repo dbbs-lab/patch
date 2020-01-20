e7835b0ded9e96219245d1f71358ee264e24c3d5
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-20 14:53:20 +0100

    Redirects all output to a stream to catch NEURON errors

--- patch/core.py
+++ patch/core.py
@@ -1,3 +1,6 @@
+from contextlib import contextmanager
+import os, sys
+
 def transform(obj):
   '''
     Transforms an object to its NEURON representation, if the __neuron__ magic
@@ -10,3 +13,24 @@ def transform(obj):
 def is_sequence(obj):
     t = type(obj)
     return hasattr(t, '__len__') and hasattr(t, '__getitem__')
+
+@contextmanager
+def suppress_stdout(stream=None):
+    """
+        Makes NEURON shut the fuck up.
+    """
+    close = False
+    if stream is None:
+        stream = open(os.devnull, "w")
+        close = True
+    old_stdout = sys.stdout
+    old_stderr = sys.stderr
+    sys.stdout = stream
+    sys.stderr = stream
+    try:
+        yield
+    finally:
+        if close:
+            stream.close()
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
