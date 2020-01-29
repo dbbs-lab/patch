2dcd270a79569ddaa780930e8aba65780f0487b4
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-29 22:34:19 +0100

    switched to new error handling

--- patch/core.py
+++ patch/core.py
@@ -27,25 +27,3 @@ def transform_record(obj):
 def _is_sequence(obj):
     t = type(obj)
     return hasattr(t, "__len__") and hasattr(t, "__getitem__")
-
-
-@contextmanager
-def _suppress_stdout(stream=None):
-    """
-        Makes NEURON shut the fuck up.
-    """
-    close = False
-    if stream is None:
-        stream = open(os.devnull, "w")
-        close = True
-    old_stdout = sys.stdout
-    old_stderr = sys.stderr
-    sys.stdout = stream
-    sys.stderr = stream
-    try:
-        yield
-    finally:
-        if close:
-            stream.close()
-        sys.stdout = old_stdout
-        sys.stderr = old_stderr
