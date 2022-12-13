84d00c5afaef1d22f79706af170e27ab081cb187
Robin De Schepper <robin.deschepper93@gmail.com>
2022-12-13 12:34:50 +0100

    bump

--- patch/__init__.py
+++ patch/__init__.py
@@ -26,7 +26,7 @@ class PythonHocModule(types.ModuleType):
     transform_record = staticmethod(transform_record)
     transform_arc = staticmethod(transform_arc)
 
-    __version__ = "4.0.0a0"
+    __version__ = "4.0.0a1"
     __path__ = __path__
 
     @property
