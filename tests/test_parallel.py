492f3d96a8be77b530b135034ddef7efa53b70c0
Robin De Schepper <robin.deschepper93@gmail.com>
2023-01-02 14:29:01 +0100

    test parallel patch across nodes

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -140,6 +140,9 @@ class TestParallelPointProcess(_shared.NeuronTestCase):
         return h.Vector().record(h._ref_t), h.Vector().record(my_s(0.5)._ref_v)
 
     def test_synapse(self):
+        """
+        Tests whether spikes get transferred across nodes
+        """
         tpp, vpp = self._setup_synapse_parallel()
         tps, vps = self._setup_synapse_patch()
         tn, vn = self._setup_synapse_neuron()
@@ -159,3 +162,81 @@ class TestParallelPointProcess(_shared.NeuronTestCase):
             self.assertEqual(
                 [*vpp], [*vps], "Outcome differences in serial/parallel comparison"
             )
+
+    # noinspection DuplicatedCode
+    def _setup_neuron_across_nodes(self):
+        from neuron import h
+
+        pc = h.ParallelContext()
+        if not pc.id():
+            my_s = h.Section()
+            syn_ = h.ExpSyn(my_s(0.5))
+            syn_.tau = 2
+            stim = h.NetStim()
+            stim.number = 5
+            stim.start = 0
+            stim.interval = 10
+            ncstim = h.NetCon(stim, syn_)
+            ncstim.weight[0] = 0.04
+            ncstim.delay = 1
+            nc_emit = h.NetCon(my_s(0.5)._ref_v, None, sec=my_s)
+            nc_emit.threshold = -52
+            nc_emit.delay = 1
+            pc.set_gid2node(3032, pc.id())
+            pc.cell(3032, nc_emit)
+            pc.outputcell(3032)
+            self._store3 = [my_s, stim, syn_, ncstim, nc_emit]
+
+        my_s2 = h.Section()
+        syn_2 = h.ExpSyn(my_s2(0.5))
+        syn_2.tau = 2
+        nc_recv = pc.gid_connect(3032, syn_2)
+        nc_recv.weight[0] = 0.04
+        self._store4 = [my_s2, syn_2, nc_recv]
+        return h.Vector().record(h._ref_t), h.Vector().record(my_s2(0.5)._ref_v)
+
+    # noinspection DuplicatedCode
+    def _setup_patch_across_nodes(self):
+        from patch import p
+
+        pc = p.parallel
+        if not pc.id():
+            my_s = p.Section()
+            syn_ = my_s.synapse(p.ExpSyn, attributes={"tau": 2})
+            syn_.stimulate(number=5, start=0, interval=10, weight=0.04, delay=1)
+            p.ParallelCon(my_s, 3033, threshold=-52, delay=1)
+
+        my_s2 = p.Section()
+        syn_2 = my_s2.synapse(p.ExpSyn, attributes={"tau": 2})
+        nc_recv = p.ParallelCon(3033, syn_2)
+        # Assert https://github.com/neuronsimulator/nrn/issues/2135
+        with self.assertRaises(RuntimeError):
+            nc_recv.threshold = -20
+        nc_recv.weight[0] = 0.04
+        return p.time, p.record(my_s2(0.5)._ref_v)
+
+    def test_same_node_synapse(self):
+        """
+        Tests whether spikes get transferred across nodes
+        """
+        tps, vps = self._setup_patch_across_nodes()
+        tpn, vpn = self._setup_neuron_across_nodes()
+        import numpy as np
+        from patch import p
+
+        spt, spid = p.parallel.spike_record()
+        p.parallel._warn_new_gids = False
+
+        p.run(100)
+
+        spikes = [3032, 3033] if not p.parallel.id() else []
+        print(list(spid))
+        self.assertEqual(len(spikes), len(spid), "expected 2 on main, 0 elsewhere")
+        self.assertEqual(spikes, sorted(list(spid)), "expected 2 on main, 0 elsewhere")
+        arr = np.array(p.parallel.py_allgather(list(vps)))
+        self.assertGreater(arr.shape[1], 0, "No signal recorded")
+        self.assertTrue(np.allclose(np.diff(arr, axis=0), 0), "Diff output on diff nodes")
+        ar2 = np.array(p.parallel.py_allgather(list(vpn)))
+        self.assertGreater(ar2.shape[1], 0, "No signal recorded")
+        self.assertTrue(np.allclose(np.diff(ar2, axis=0), 0), "Diff output on diff nodes")
+        self.assertTrue(np.allclose(arr, ar2), "NEURON vs Patch differences")
