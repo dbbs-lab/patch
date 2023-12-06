d37ebcb16b23e10925f370966e3f82ab097b5c4c
Robin De Schepper <robin.deschepper93@gmail.com>
2023-12-06 17:39:24 +0100

    switch to nmodl-glia

--- patch/__init__.py
+++ patch/__init__.py
@@ -27,7 +27,7 @@ from .core import (
 from .exceptions import NotConnectableError, NotConnectedError
 from .interpreter import PythonHocInterpreter
 
-__version__ = "4.0.0b0"
+__version__ = "4.0.0b1"
 p: "PythonHocInterpreter"
 h: "PythonHocInterpreter"
 
