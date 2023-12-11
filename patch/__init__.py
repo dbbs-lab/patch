1ca2d645897ff3d40e1b101460a9a5c9c61f0eb3
Robin De Schepper <robin.deschepper93@gmail.com>
2023-12-11 22:44:05 +0100

    store wrapped pointer parameters on class

--- patch/__init__.py
+++ patch/__init__.py
@@ -27,7 +27,7 @@ from .core import (
 from .exceptions import NotConnectableError, NotConnectedError
 from .interpreter import PythonHocInterpreter
 
-__version__ = "4.0.0b1"
+__version__ = "4.0.0b3"
 p: "PythonHocInterpreter"
 h: "PythonHocInterpreter"
 
